eda35641a8b6155fe47d79b7f1df73450beb2217
dfarhi <32961271+dfarhi@users.noreply.github.com>
2022-06-03 17:55:36 -0700

    ts worker track crashes

--- minionsai/run_game.py
+++ minionsai/run_game.py
@@ -2,6 +2,11 @@ from typing import Tuple
 from .engine import Game
 from .agent import Agent
 
+class AgentException(Exception):
+    def __init__(self, error, agent_index):
+        self.error = error
+        self.agent_index = agent_index
+
 def run_game(game: Game, agents: Tuple[Agent, Agent], verbose=False) -> int:
     while True:
         if verbose:
@@ -14,7 +19,11 @@ def run_game(game: Game, agents: Tuple[Agent, Agent], verbose=False) -> int:
             break
         active_agent = agents[game.active_player_color]
         game_copy = game.copy()
-        actionlist = active_agent.act(game_copy)
+        try:
+            actionlist = active_agent.act(game_copy)
+        except Exception as e:
+            print("Error in agent's act() function!")
+            raise AgentException(e, game.active_player_color)
         if verbose:
             print(actionlist)
         game.full_turn(actionlist)
--- minionsai/trueskill_worker.py
+++ minionsai/trueskill_worker.py
@@ -1,8 +1,8 @@
 
 import datetime
-from typing import Callable
+import traceback
+from typing import Callable, Optional
 import random
-from xml.sax.handler import property_encoding
 import tqdm
 import trueskill
 import os
@@ -10,7 +10,7 @@ from tabulate import tabulate
 import csv
 
 from .agent import Agent
-from .run_game import run_game
+from .run_game import AgentException, run_game
 from .engine import Game
 
 def get_scores_file(directory):
@@ -28,11 +28,10 @@ def read_scores(env_dir):
         reader = csv.reader(f)
         # first line is header; check that it matches our expectations
         header = next(reader)
-        if header != ['name', 'trueskill', 'trueskill_sigma', 'games_played']:
+        if header != ['name', 'trueskill', 'trueskill_sigma', 'games_played', 'ok', 'crashes']:
             raise Exception(f"Unexpected header in {scores_file}: {header}")
         scores = list(reader)
-    # reformat to {'name': name, 'trueskill': trueskill, 'trueskill_sigma': trueskill_sigma, 'games_played': games_played}
-    scores = [{'name': s[0], 'trueskill': float(s[1]), 'trueskill_sigma': float(s[2]), 'games_played': int(s[3])} for s in scores]
+    scores = [{'name': s[0], 'trueskill': float(s[1]), 'trueskill_sigma': float(s[2]), 'games_played': int(s[3]), 'load_ok': s[4], 'crashes': int(s[5])} for s in scores]
     last_update = datetime.datetime.fromtimestamp(os.path.getmtime(scores_file))
     return scores, last_update
 
@@ -47,6 +46,8 @@ class TrueskillWorker():
         self.choose_underevaluated_agents_prob = 0.5
         self.batch_size = batch_size
         self.env = trueskill.TrueSkill(tau=0, draw_probability=0.0)
+        self.dropped_agents = []
+        self.max_crash_rate = 0.5
 
         # Restart from from where the last thread stopped if possible.
         prev_scores, _ = read_scores(directory)
@@ -60,6 +61,7 @@ class TrueskillWorker():
         self.ratings = {agent_dict['name']: trueskill.Rating(agent_dict['trueskill'], agent_dict['trueskill_sigma']) 
                         for agent_dict in prev_scores}
         self.num_games = {agent_dict['name']: agent_dict['games_played'] for agent_dict in prev_scores}
+        self.num_crashes = {agent_dict['name']: agent_dict['crashes'] for agent_dict in prev_scores}
 
     def run(self):
         while True:
@@ -77,14 +79,32 @@ class TrueskillWorker():
             print("Agent", agent_name, "is gone from directory.")
             self.agent_names.remove(agent_name)
 
-        for agent_name in set(directory_agents) - set(self.agent_names):
+        for agent_name in set(directory_agents) - set(self.agent_names) - set(self.dropped_agents):
             print(f"Found new agent: {agent_name}")
             self.agent_names.append(agent_name)
             self.ratings[agent_name] = trueskill.Rating()
             self.num_games[agent_name] = 0
+            self.num_crashes[agent_name] = 0
 
-    def load_agent(self, agent_name: str) -> Agent:
-        return Agent.load(os.path.join(self.directory, agent_name))
+    def load_agent(self, agent_name: str) -> Optional[Agent]:
+        try:
+            return Agent.load(os.path.join(self.directory, agent_name))
+        except Exception as e:
+            print(f"Error loading agent: {agent_name}")
+            print(e)
+
+            timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            stack_trace_file = os.path.join(self.directory, agent_name, f"stacktrace_load_{timestamp}.txt")
+            print(f"Saving stack trace to: {stack_trace_file}")
+            with open(stack_trace_file, "w") as f:
+                traceback.print_exc(file=f)
+            print("I'm going to stop worrying about that agent.")
+            self.drop_agent(agent_name)
+            return None
+
+    def drop_agent(self, agent_name: str):
+        self.dropped_agents.append(agent_name)
+        self.agent_names.remove(agent_name)
 
     @property
     def scores_file(self):
@@ -94,23 +114,27 @@ class TrueskillWorker():
         # Save a csv with columns ['name', 'trueskill', 'trueskill_sigma', 'games_played']
         with open(self.scores_file, "w", newline='') as f:
             writer = csv.writer(f)
-            writer.writerow(["name", "trueskill", "trueskill_sigma", "games_played"])
+            writer.writerow(["name", "trueskill", "trueskill_sigma", "games_played", "ok", "crashes"])
             for name in self.agent_names:
-                writer.writerow([name, self.ratings[name].mu, self.ratings[name].sigma, self.num_games[name]])
+                writer.writerow([name, self.ratings[name].mu, self.ratings[name].sigma, self.num_games[name], True, self.num_crashes[name]])
+            for name in self.dropped_agents:
+                writer.writerow([name, self.ratings[name].mu, self.ratings[name].sigma, self.num_games[name], False, self.num_crashes[name]])
 
     def print_ratings(self):
         table = []
         for name in self.agent_names:
-            table.append([name, self.ratings[name].mu, self.ratings[name].sigma, self.num_games[name]])
+            table.append([name, self.ratings[name].mu, self.ratings[name].sigma, self.num_games[name], True, self.num_crashes[name]])
+        for name in self.dropped_agents:
+            table.append([name, self.ratings[name].mu, self.ratings[name].sigma, self.num_games[name], False, self.num_crashes[name]])
         table.sort(key=lambda x: x[1], reverse=True)
-        #Print with 2 decimal places
-        return tabulate(table, headers=["Name", "Mu", "Sigma", "Games"], floatfmt=".1f")
+        return tabulate(table, headers=["Name", "Mu", "Sigma", "Games", "OK", 'Crashes'], floatfmt=".1f")
 
     def choose_agents(self):
+        print(f"Choosing among agents {self.agent_names}")
         if random.random() < self.choose_underevaluated_agents_prob:
             # Make sure to choose one agent with minimal num_games
-            min_num_games = min(self.num_games.values())
-            min_games_agent_names = [name for name in self.num_games if self.num_games[name] == min_num_games]
+            min_num_games = min([self.num_games[a] for a in self.agent_names])
+            min_games_agent_names = [name for name in self.agent_names if self.num_games[name] == min_num_games]
             first_agent = random.choice(min_games_agent_names)
             other_agents = [name for name in self.agent_names if name != first_agent]
             second_agent = random.choice(other_agents)
@@ -121,13 +145,9 @@ class TrueskillWorker():
     def play_games(self):
         agent_names = self.choose_agents()
         print(f"Playing games between {agent_names}")
-        try:
-            agent_0 = self.load_agent(agent_names[0])
-            agent_1 = self.load_agent(agent_names[1])
-            agents = [agent_0, agent_1]
-        except Exception as e:
-            print(f"Error loading agents:")
-            print(e)
+        agents = [self.load_agent(a) for a in agent_names]
+        if None in agents:
+            print("One or more agents failed to load. Skipping this round.")
             return
 
         wins = {name: 0 for name in agent_names}
@@ -136,7 +156,30 @@ class TrueskillWorker():
             player0 = i % 2
             agents_shuffled = [agents[player0], agents[1 - player0]]
             agent_names_shuffled = [agent_names[player0], agent_names[1 - player0]]
-            winner = run_game(game, agents=agents_shuffled, verbose=False)
+            try:
+                winner = run_game(game, agents=agents_shuffled, verbose=False)
+            except AgentException as e:
+                agent_index = e.agent_index
+                offender = agent_names_shuffled[agent_index]
+                print(f"Agent {offender} threw an exception during game {i}")
+                timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+                stack_trace_file = os.path.join(self.directory, offender, f"stacktrace_play_{timestamp}.txt")
+                print(f"Saving stack trace to: {stack_trace_file}")
+                with open(stack_trace_file, "w") as f:
+                    traceback.print_exc(file=f)
+
+                self.num_crashes[offender] += 1
+
+                # If the agent crashed too many times, drop it
+                enough_games = self.num_crashes[offender] >= self.batch_size * 3
+                too_many_crashes =self.num_crashes[offender] / (self.num_games[offender] + self.num_crashes[offender]) > self.max_crash_rate
+                print(f"Enough games: {enough_games}, too many crashes: {too_many_crashes}")
+                if enough_games and too_many_crashes:
+                    print(f"Agent {offender} has crashed too many times. Dropping it.")
+                    self.drop_agent(offender)
+                    break
+                continue
+
             winner_name = agent_names_shuffled[winner]
             loser_name = agent_names_shuffled[1 - winner]
             winner_rating = self.ratings[winner_name]
