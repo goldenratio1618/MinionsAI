2112bda481787374b2b8fc3ad6fbf8478b8d40f0
dfarhi <32961271+dfarhi@users.noreply.github.com>
2022-06-05 16:59:38 -0700

    Play vs agent argparsing

--- scripts/play_vs_agent.py
+++ scripts/play_vs_agent.py
@@ -6,6 +6,9 @@ import sys
 from minionsai.agent import Agent, HumanCLIAgent, RandomAIAgent
 from minionsai.run_game import run_game
 from minionsai.engine import Game
+import argparse
+import tempfile
+import os
 
 def main(agent_dir=None):
     if agent_dir is None:
@@ -18,5 +21,34 @@ def main(agent_dir=None):
     print("Game over.\nWinner:", winner)
 
 if __name__ == "__main__":
-    arg = sys.argv[1] if len(sys.argv) >= 2 else None
-    main(arg)
+    # Use argparse to parse arguments "path", "name" and "iter"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", help="Full path to agent directory")
+    parser.add_argument("--name", help="Experiment name (will automatically find path in <tempdir>/MinionsAI/<name>/checkpoints)")
+    parser.add_argument("--iter", help="Iteration number (if unspecified, uses latest)")
+    args = parser.parse_args()
+
+    if args.iter is not None:
+        assert args.path is None, "Cannot specify both --path and --iter"
+        assert args.name is not None, "Must specify --name when using --iter"
+
+    if args.path is not None:
+        assert args.name is None, "Cannot specify both --path and --name"
+        path = args.path
+    elif args.name is not None:
+        agents_dir = os.path.join(tempfile.gettempdir(), "MinionsAI", args.name, "checkpoints")
+        print(f"Finding path by experiment name: {agents_dir}")
+        if args.iter is None:
+            print("Finding latest iter ...")
+            # Find the latest iteration based on the filenames
+            choices = os.listdir(agents_dir)
+            choices_iters = [int(choice.split('_')[-1]) for choice in choices]
+            print(choices_iters)
+            iter = max(choices_iters)
+        else:
+            print(f"Using specified iter: {args.iter}")
+            iter = args.iter
+        path = os.path.join(agents_dir, f"iter_{iter}")
+    print(f"Getting agent from: {path}")
+
+    main(path)
