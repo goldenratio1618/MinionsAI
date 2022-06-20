9d27ba2147d990983e832c473f8eca2648034392
dfarhi <32961271+dfarhi@users.noreply.github.com>
2022-06-19 21:28:00 -0700

    better UI for disc_mode

--- minionsai/gen_disc/discriminators.py
+++ minionsai/gen_disc/discriminators.py
@@ -1,8 +1,9 @@
 import abc
+from audioop import reverse
 from typing import List
 
 from ..game_util import adjacent_zombies, equal_np_dicts, stack_dicts
-from ..unit_type import NECROMANCER, ZOMBIE
+from ..unit_type import NECROMANCER, ZOMBIE, flexible_unit_type
 from ..engine import Game, dist, print_n_games
 
 import numpy as np
@@ -71,10 +72,41 @@ class HumanDiscriminator(BaseDiscriminator):
             sorted_idxes = list(range(len(games)))
             logprobs = [None] * len(games)
 
-        for r in range(self.nrows):
+        return self.display_and_choose(sorted_idxes, logprobs, games)
+
+    def display_and_choose(self, sorted_idxes, logprobs, games):
+        for r in range(min(self.nrows, len(sorted_idxes) // self.ncols + 1)):
             print("-" * 16 * self.ncols)
             idxs = sorted_idxes[r * self.ncols : (r + 1) * self.ncols]
             print_n_games([games[i] for i in idxs])
             print("".join([f"Opt {i:<3} ({th.sigmoid(logprobs[i]).item():.1%})".ljust(15)+"|" for i in idxs]))
 
-        return int(input("Choose an option: "))
+        print("-" * 16 * self.ncols)
+        print(f"{len(sorted_idxes)} unique choices of {len(games)} total generated.")
+        option = input("Choose an option: ")
+        # If it's a valid number less than 256, it's a choice and we return it.
+        if option.isdigit() and int(option) < len(games):
+            return int(option)
+        # If it starts with "f" or "filter" then it's a request to filter the games
+        elif option.startswith("f") or option.startswith("filter"):
+            try:
+                _f, unit_type, i, j = option.split(" ")
+                i, j = int(i), int(j)
+                unit_type = flexible_unit_type(unit_type)
+                # resort indices by whether the unit is at the given location
+                def filter(game):
+                    unit = game.board.board[i][j].unit
+                    return unit is not None and unit.type == unit_type
+                resorted_idxes = [i for i in sorted_idxes if filter(games[i])]
+                return self.display_and_choose(resorted_idxes, logprobs, games)
+            except Exception as e:
+                print("Invalid filter request")
+                print(e)
+                return self.display_and_choose(sorted_idxes, logprobs, games)
+        # If it starts with "c" or "clear" then it's a request to clear the filters
+        elif option.startswith("c") or option.startswith("clear"):
+            return self.choose_option(games)            
+        else:
+            print("Invalid option")
+            return self.display_and_choose(sorted_idxes, logprobs, games)
+
