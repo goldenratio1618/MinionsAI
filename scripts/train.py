9caf27300393545053c949d778339284de2e11bc
dfarhi <32961271+dfarhi@users.noreply.github.com>
2022-06-26 09:47:21 -0700

    TRAIN_DISCRIMINATOR

--- scripts/train.py
+++ scripts/train.py
@@ -30,7 +30,8 @@ from minionsai.metrics_logger import metrics_logger
 
 logger = logging.getLogger(__name__)
 
-TRAIN_GENERATOR = True
+TRAIN_GENERATOR = False
+TRAIN_DISCRIMINATOR = True
 
 # How many rollouts do we run of each turn before picking the best
 ROLLOUTS_PER_TURN = 64
@@ -41,7 +42,7 @@ GEN_SAMPLING_TEMPERATURE = 0.01
 # How many episodes of data do we collect each iteration, before running a few epochs of optimization?
 # Potentially good to use a few times bigger EPISODES_PER_ITERATION than BATCH_SIZE, to minimize correlation within batches
 EPISODES_PER_ITERATION = 256
-ROLLOUT_PROCS = 1
+ROLLOUT_PROCS = 4
 
 # Once we've collected the data, how many times do we go over it for optimization (within one iteration)?
 SAMPLE_REUSE = 2
@@ -52,12 +53,11 @@ EVAL_EVERY = 8
 EVAL_VS_PAST_ITERS = []
 # Specific agent instances to eval vs
 EVAL_VS_AGENTS = [
-    RandomAIAgent()
     # GenDiscAgent(ScriptedDiscriminator(), RandomAIAgent(), rollouts_per_turn=16),
     # os.path.join(get_experiments_directory(), "conv_big", "checkpoints", "iter_200")
 ]
 # Eval against random up until this iteration
-EVAL_VS_RANDOM_UNTIL = 0
+EVAL_VS_RANDOM_UNTIL = 20
 EVAL_TRIALS = 100
 
 # Frequency of storing a saved agent
@@ -98,15 +98,19 @@ def build_agent():
         generator = AgentGenerator(RandomAIAgent())
 
     logger.info("Creating discriminator...")
-    disc_model = MinionsDiscriminator(d_model=D_MODEL, depth=DEPTH)
-    logger.info("Discriminator model initialized:")
-    logger.info(disc_model)
-    logger.info(f"Discriminator model total parameter count: {sum(p.numel() for p in disc_model.parameters() if p.requires_grad):,}")
+    if TRAIN_DISCRIMINATOR:
+        disc_model = MinionsDiscriminator(d_model=D_MODEL, depth=DEPTH)
+        logger.info("Discriminator model initialized:")
+        logger.info(disc_model)
+        logger.info(f"Discriminator model total parameter count: {sum(p.numel() for p in disc_model.parameters() if p.requires_grad):,}")
 
-    disc_translator = Translator("discriminator")
-    discriminator = QDiscriminator(translator=disc_translator, model=disc_model, epsilon_greedy=DISC_EPSILON_GREEDY)
+        disc_translator = Translator("discriminator")
+        discriminator = QDiscriminator(translator=disc_translator, model=disc_model, epsilon_greedy=DISC_EPSILON_GREEDY)
+    else:
+        discriminator = ScriptedDiscriminator()
 
-    logger.info(f"Discriminator model total parameter count: {sum(p.numel() for p in disc_model.parameters() if p.requires_grad):,}")
+    if TRAIN_DISCRIMINATOR:
+        logger.info(f"Discriminator model total parameter count: {sum(p.numel() for p in disc_model.parameters() if p.requires_grad):,}")
     if TRAIN_GENERATOR:
             logger.info(f"Generator model total parameter count: {sum(p.numel() for p in gen_model.parameters() if p.requires_grad):,}")
 
@@ -124,25 +128,30 @@ def eval_vs_other(agent, eval_agent, name):
     # Hack to temporarily change the agent's rollouts_per_turn & epsilon greedy values
     # TODO - make it easier to set an agent into "eval" mode.
     agent.rollouts_per_turn = ROLLOUTS_PER_TURN * EVAL_COMPUTE_BOOST
-    agent.discriminator.epsilon_greedy = 0.0
+    if TRAIN_DISCRIMINATOR:
+        agent.discriminator.epsilon_greedy = 0.0
     wins, _metrics = run_n_games(ENVS[EVAL_ENV_NAME], [agent, eval_agent], n=EVAL_TRIALS)
     agent.rollouts_per_turn = ROLLOUTS_PER_TURN
-    agent.discriminator.epsilon_greedy = DISC_EPSILON_GREEDY
+    if TRAIN_DISCRIMINATOR:
+        agent.discriminator.epsilon_greedy = DISC_EPSILON_GREEDY
     winrate = wins[0] / EVAL_TRIALS
     metrics_logger.log_metrics({f"eval_winrate/{name}": winrate})
     logger.info(f"Win rate vs {name} = {winrate}")  
 
 def main(run_name):
     seed_everything(SEED)
+    assert TRAIN_DISCRIMINATOR or TRAIN_GENERATOR, "What are you doing?"
+
     checkpoint_dir, code_dir = setup_directory(run_name)
     logger.info(f"Starting run {run_name}")
 
     device = find_device()
 
     agent = build_agent()
-    disc_model = agent.discriminator.model
-    disc_model.to(device)
-    disc_optimizer = th.optim.Adam(disc_model.parameters(), lr=DISC_LR)
+    if TRAIN_DISCRIMINATOR:
+        disc_model = agent.discriminator.model
+        disc_model.to(device)
+        disc_optimizer = th.optim.Adam(disc_model.parameters(), lr=DISC_LR)
 
     if TRAIN_GENERATOR:
         gen_model = agent.generator.model
@@ -150,12 +159,14 @@ def main(run_name):
         gen_optimizer = th.optim.Adam(gen_model.parameters(), lr=GEN_LR)
 
     def model_mode_eval():
-        disc_model.eval()
+        if TRAIN_DISCRIMINATOR:
+            disc_model.eval()
         if TRAIN_GENERATOR:
             gen_model.eval()
 
     def model_mode_train():
-        disc_model.train()
+        if TRAIN_DISCRIMINATOR:
+            disc_model.train()
         if TRAIN_GENERATOR:
             gen_model.train()
 
@@ -166,6 +177,7 @@ def main(run_name):
 
     iteration = 0
     turns_optimized = 0
+    gen_actions_optimized = 0
     rollout_stats = defaultdict(int)
     while MAX_ITERATIONS is None or iteration <= MAX_ITERATIONS:
         with metrics_logger.timing('iteration'):
@@ -184,14 +196,14 @@ def main(run_name):
 
             metrics_logger.log_metrics(rollout_stats)
             disc_param_norm = sum([th.norm(param, p=2) for param in disc_model.parameters()]).item()
-            metrics_logger.log_metrics({
-                    'disc/epsilon_greedy': agent.discriminator.epsilon_greedy, 
-                    'turns_optimized': turns_optimized, 
-                    'disc/param_norm': disc_param_norm})
+            if TRAIN_DISCRIMINATOR:
+                metrics_logger.log_metrics({
+                        'disc/turns_optimized': turns_optimized, 
+                        'disc/param_norm': disc_param_norm})
             if TRAIN_GENERATOR:
                 gen_param_norm = sum([th.norm(param, p=2) for param in gen_model.parameters()]).item()
                 metrics_logger.log_metrics({
-                    'gen/epsilon_greedy': agent.generator.epsilon_greedy, 
+                    'gen/actions_optimized': gen_actions_optimized,
                     'gen/param_norm': gen_param_norm})
 
             with metrics_logger.timing('rollouts'):
@@ -204,31 +216,32 @@ def main(run_name):
                 rollout_stats['rollouts/games'] += rollout_batch['discriminator'].num_games
                 rollout_stats['rollouts/turns'] += num_turns
 
-            with metrics_logger.timing('training/disc'):
-                logger.info("Starting training discriminator...")
-                disc_rollout_batch = rollout_batch['discriminator']
-                for epoch in range(SAMPLE_REUSE):
-                    logger.info(f"  Epoch {epoch}/{SAMPLE_REUSE}...")
-                    all_idxes = np.random.permutation(num_turns)
-                    n_batches = num_turns // BATCH_SIZE
-                    for idx in range(n_batches):
-                        with metrics_logger.timing('training_batch'):
-                            batch_idxes = all_idxes[idx * BATCH_SIZE: (idx + 1) * BATCH_SIZE]
-                            batch_obs = {}
-                            for key in disc_rollout_batch.obs:
-                                batch_obs[key] = disc_rollout_batch.obs[key][batch_idxes]
-                            batch_labels = disc_rollout_batch.labels[batch_idxes]
-                            batch_labels = th.from_numpy(batch_labels).to(device)
-                            disc_optimizer.zero_grad()
-                            disc_logprob = disc_model(batch_obs) # [batch, 1]
-                            batch_labels = th.unsqueeze(batch_labels, 1)
-                            loss = th.nn.BCEWithLogitsLoss()(disc_logprob, batch_labels)
-                            loss.backward()
-                            disc_optimizer.step()
-                            if idx in [0, n_batches // 2, n_batches - 1]:
-                                max_batch_digits = len(str(n_batches))
-                                metrics_logger.log_metrics({f"loss/epoch_{epoch}/batch_{idx:0>{max_batch_digits}}": loss.item()})
-                            turns_optimized += len(batch_idxes)
+            if TRAIN_DISCRIMINATOR:
+                with metrics_logger.timing('training/disc'):
+                    logger.info("Starting training discriminator...")
+                    disc_rollout_batch = rollout_batch['discriminator']
+                    for epoch in range(SAMPLE_REUSE):
+                        logger.info(f"  Epoch {epoch}/{SAMPLE_REUSE}...")
+                        all_idxes = np.random.permutation(num_turns)
+                        n_batches = num_turns // BATCH_SIZE
+                        for idx in range(n_batches):
+                            with metrics_logger.timing('training_batch'):
+                                batch_idxes = all_idxes[idx * BATCH_SIZE: (idx + 1) * BATCH_SIZE]
+                                batch_obs = {}
+                                for key in disc_rollout_batch.obs:
+                                    batch_obs[key] = disc_rollout_batch.obs[key][batch_idxes]
+                                batch_labels = disc_rollout_batch.labels[batch_idxes]
+                                batch_labels = th.from_numpy(batch_labels).to(device)
+                                disc_optimizer.zero_grad()
+                                disc_logprob = disc_model(batch_obs) # [batch, 1]
+                                batch_labels = th.unsqueeze(batch_labels, 1)
+                                loss = th.nn.BCEWithLogitsLoss()(disc_logprob, batch_labels)
+                                loss.backward()
+                                disc_optimizer.step()
+                                if idx in [0, n_batches // 2, n_batches - 1]:
+                                    max_batch_digits = len(str(n_batches))
+                                    metrics_logger.log_metrics({f"loss/epoch_{epoch}/batch_{idx:0>{max_batch_digits}}": loss.item()})
+                                turns_optimized += len(batch_idxes)
             # TODO - train genereator here
             logger.info(f"Iteration {iteration} complete.")
             iteration += 1
