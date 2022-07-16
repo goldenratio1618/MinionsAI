e0a19421675ce9017923db30c91fb497f1d4fd4d
dfarhi <32961271+dfarhi@users.noreply.github.com>
2022-07-16 09:00:21 -0700

    wip (identical)

--- minionsai/multiprocessing_rl/rollouts.py
+++ minionsai/multiprocessing_rl/rollouts.py
@@ -1,6 +1,7 @@
 import abc
 from collections import defaultdict
 from typing import Dict
+from minionsai.game_util import stack_dicts
 import numpy as np
 import tqdm
 
@@ -20,6 +21,7 @@ class OptimizerRolloutSource(abc.ABC):
 
     def get_rollouts(self, iteration: int) -> Dict[str, RolloutBatch]:
         disc_obs = []
+        disc_next_obs = []
         disc_labels = []
 
         gen_obs = []
@@ -42,6 +44,8 @@ class OptimizerRolloutSource(abc.ABC):
 
                 rollout_episode = self.next_rollout(iteration, idx)
                 disc_obs.extend(rollout_episode.disc_obs)
+                disc_null_obs = {k: np.zeros_like(v) for k, v in rollout_episode.disc_obs[0].items()}
+                disc_next_obs.extend(rollout_episode.disc_obs[1:] + [disc_null_obs])
                 disc_labels.extend(rollout_episode.disc_labels)
                 gen_obs.append(rollout_episode.gen_obs)
                 assert rollout_episode.gen_actions == [] or rollout_episode.gen_actions.shape == (rollout_episode.gen_labels.shape[0], 2)
@@ -55,8 +59,9 @@ class OptimizerRolloutSource(abc.ABC):
                     global_metrics_accumulated[key].append(rollout_episode.global_metrics[key])
         # convert from list of dicts of arrays to a single dict of arrays with large batch dimension
         if len(disc_obs) > 0:
-            disc_obs = {k: np.concatenate([s[k] for s in disc_obs], axis=0) for k in disc_obs[0]}
-            disc_obs, disc_labels, _ = add_symmetries(disc_obs, disc_labels, None)
+            disc_obs = stack_dicts(disc_obs)
+            disc_next_obs = stack_dicts(disc_next_obs)
+            disc_obs, disc_next_obs, disc_labels, _ = add_symmetries(disc_obs, disc_next_obs, disc_labels, None)
 
         # gen_obs is a list of length episodes_per_iteration; 
         # If we aren't training the generator, they're all empty
@@ -65,15 +70,29 @@ class OptimizerRolloutSource(abc.ABC):
             gen_labels = np.concatenate(gen_labels, axis=0)
             gen_actions = np.concatenate(gen_actions, axis=0)
             assert gen_actions.shape == (gen_labels.shape[0], 2), gen_actions.shape
-            gen_obs, gen_labels, gen_actions = add_symmetries(gen_obs, gen_labels, gen_actions)
+            gen_obs, gen_next_obs, gen_labels, gen_actions = add_symmetries(gen_obs, {}, gen_labels, gen_actions)
             assert gen_actions.shape == (gen_labels.shape[0], 2), gen_actions.shape
 
         metrics_logger.log_metrics({k: sum(v)/self.episodes_per_iteration for k, v in global_metrics_accumulated.items()}, prefix=f'rollouts/game')
         for color in (0, 1):
             metrics_logger.log_metrics({k: sum(v)/self.episodes_per_iteration for k, v in player_metrics_accumulated[color].items()}, prefix=f'rollouts/game/{color}')
         return {
-            "discriminator": RolloutBatch(disc_obs, None, disc_labels, num_games=self.episodes_per_iteration),
-            "generator": RolloutBatch(gen_obs, gen_actions, gen_labels, num_games=self.episodes_per_iteration),
+            "discriminator": RolloutBatch(
+                obs=disc_obs, 
+                next_obs=disc_next_obs, 
+                actions=None, 
+                next_maxq=disc_labels, 
+                terminal_action=None,
+                reward=None,
+            ),
+            "generator": RolloutBatch(
+                obs=gen_obs, 
+                next_obs=gen_next_obs, 
+                actions=gen_actions, 
+                next_maxq=gen_labels,
+                terminal_action=None, # TODO
+                reward=None, # TODO
+            ),
         }
 
     @abc.abstractmethod
@@ -83,15 +102,16 @@ class OptimizerRolloutSource(abc.ABC):
     def launch_rollouts(self, iteration: int, hparams: Dict) -> None:
         pass
 
-def add_symmetries(obs, labels, actions=None):
+def add_symmetries(obs, next_obs, labels, actions=None):
     """
     Add symmetries to the observations and labels.
     """
     symmetrized_obs, symmetrized_actions = Translator.symmetries(obs, actions)
     # Now combine them into one big states dict
-    obs = {k: np.concatenate([s[k] for s in symmetrized_obs], axis=0) for k in obs}
+    obs = stack_dicts(symmetrized_obs)
     labels = np.concatenate([labels]*len(symmetrized_obs), axis=0)
-    return obs, labels, symmetrized_actions
+    next_obs = stack_dicts([next_obs] * len(symmetrized_obs))
+    return obs, next_obs, labels, symmetrized_actions
 
 class InProcessRolloutSource(OptimizerRolloutSource):
     """
